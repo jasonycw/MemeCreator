c6305b6dc98e12a233d811c3717fdcca9aeb85b3
Jason Yu <jasonycw1992@gmail.com>
2014-04-09 12:52:44 +0800

    [WelcomeScreenActivity] Setup background color [WelcomeScreenActivity] Hide action bar [TODO] Add image to the middle [TODO] Adjust the color

--- MemeCreator/src/cs4295/memecreator/WelcomeScreenAcivity.java
+++ MemeCreator/src/cs4295/memecreator/WelcomeScreenAcivity.java
@@ -1,7 +1,7 @@
 package cs4295.memecreator;
 
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class WelcomeScreenAcivity extends Activity {
 
@@ -17,7 +16,8 @@ public class WelcomeScreenAcivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
