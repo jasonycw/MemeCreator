ac74f917befdc7f2a0e823f110b866c730527c17
Jason Yu <jasonycw1992@gmail.com>
2014-04-13 13:30:19 +0800

    [FIX] only the image can be clicked blank space no longer being able to be clicked

--- MemeCreator/src/cs4295/memecreator/ResizableImageView.java
+++ MemeCreator/src/cs4295/memecreator/ResizableImageView.java
@@ -3,6 +3,7 @@ package cs4295.memecreator;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class ResizableImageView extends ImageView {
@@ -18,7 +19,14 @@ public class ResizableImageView extends ImageView {
          if(d!=null){
                  // ceil not round - avoid thin vertical gaps along the left/right edges
                  final int width = MeasureSpec.getSize(widthMeasureSpec);
+                 
+                 
+                 Log.i("Test", "getIntrinsicWidth = " + Float.toString((float) d.getIntrinsicWidth()));
+                 Log.i("Test", "getIntrinsicHeight = " + Float.toString((float) d.getIntrinsicHeight()));
                  final int height = (int) Math.ceil((float) width * (float) d.getIntrinsicHeight() / (float) d.getIntrinsicWidth());
+                 
+                 Log.i("Test", "width = " + Float.toString(width));
+                 Log.i("Test", "height = " + Float.toString(height));
                  this.setMeasuredDimension(width, height);
          }else{
                  super.onMeasure(widthMeasureSpec, heightMeasureSpec);
