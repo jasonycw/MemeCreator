84fa26cd088df57cd7987bc44f7c8e57143bc73e
HuiMing <minghui4-c@.my.cityu.edu.hk>
2014-04-18 15:59:45 +0800

    [REMOVE] Remove the path input field in setting

--- MemeCreator/src/cs4295/memecreator/SettingsActivity.java
+++ MemeCreator/src/cs4295/memecreator/SettingsActivity.java
@@ -104,7 +104,7 @@ public class SettingsActivity extends PreferenceActivity {
 		// Bind the summaries of EditText/List/Dialog/Ringtone preferences to
 		// their values. When their values change, their summaries are updated
 		// to reflect the new value, per the Android Design guidelines.
-		bindPreferenceSummaryToValue(findPreference("example_text"));
+	
 		bindPreferenceSummaryToValue(findPreference("image_size"));
 //		bindPreferenceSummaryToValue(findPreference("image_path"));
 		
@@ -215,7 +215,7 @@ public class SettingsActivity extends PreferenceActivity {
 			// to their values. When their values change, their summaries are
 			// updated to reflect the new value, per the Android Design
 			// guidelines.
-			bindPreferenceSummaryToValue(findPreference("example_text"));
+			
 			bindPreferenceSummaryToValue(findPreference("image_size"));
 //			bindPreferenceSummaryToValue(findPreference("image_path"));
 		}
